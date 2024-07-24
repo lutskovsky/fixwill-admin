a933f2883dbc5960eea39d6e2061bb4bd74c583e
Sergey Lutskovsky <s@lutskovsky.ru>
2024-07-24 23:22:21 +0300

    redirect / to /admin

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/help/{id}', function ($id) {
-    dd($id);
-
-    return view('help');
-});
+Route::redirect('/', '/admin');
