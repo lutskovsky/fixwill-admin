e193b38ec94ee1fb865e1a2c5c95dae2c0d1bdde
Sergey Lutskovsky <s@lutskovsky.ru>
2024-07-24 23:11:18 +0300

    Revert "change Backpack route to /"
    This reverts commit 6d9f4f6fde0259950d850019d0c39123ca721a38.

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-;
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/help/{id}', function ($id) {
+    dd($id);
+
+    return view('help');
+});
