e09b034809e083618c9cdf79eacb429df3a3a22e
Sergey Lutskovsky <s@lutskovsky.ru>
2024-09-06 21:16:00 +0300

    new decoding scheme

--- app/Http/Controllers/EmployeeCallController.php
+++ app/Http/Controllers/EmployeeCallController.php
@@ -51,7 +51,7 @@ class EmployeeCallController extends Controller
         $virtualNumber = $employee->virtual_number;
 
         // If the phone number was encrypted by MITM proxy
-        if (preg_match('/^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/', $contactPhoneNumber)) {
+        if (preg_match('@^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$@', $contactPhoneNumber)) {
             $contactPhoneNumber = $this->decrypt(substr($contactPhoneNumber, -44));
         }
         $contactPhoneNumber = preg_replace('/\D/', '', $contactPhoneNumber);
