ba76829692259d8ce0600f711ec6eceffe8f2df9
Sergey Lutskovsky <s@lutskovsky.ru>
2024-11-18 23:49:39 +0300

    permission seeder

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -2,26 +2,27 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
     public function run()
     {
         // Reset cached roles and permissions
-        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
         // Define roles
         $superadminRole = Role::create(['name' => 'superadmin']);
         $adminRole = Role::create(['name' => 'admin']);
-        $userRole = Role::create(['name' => 'user']);
 
 
         $createAdminPermission = Permission::create(['name' => 'create admin']);
-
         $createUserPermission = Permission::create(['name' => 'create user']);
+        $viewReports = Permission::create(['name' => 'view reports']);
 
         // Define permissions (optional)
         // For this setup, roles suffice, but permissions can be added for finer control.
@@ -30,5 +31,10 @@ class RolesAndPermissionsSeeder extends Seeder
         // Example:
         $superadminRole->givePermissionTo($createAdminPermission, $createUserPermission);
         $adminRole->givePermissionTo($createUserPermission);
+
+        $user = User::find(1);
+        $user->assignRole('superadmin');
+
+
     }
 }
