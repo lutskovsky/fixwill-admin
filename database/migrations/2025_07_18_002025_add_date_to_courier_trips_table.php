01d790625e9b935ea98ffe3bb94036343caf4df6
Sergey Lutskovsky <s@lutskovsky.ru>
2025-07-18 08:33:37 +0300

    make date nullable

--- database/migrations/2025_07_18_002025_add_date_to_courier_trips_table.php
+++ database/migrations/2025_07_18_002025_add_date_to_courier_trips_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('courier_trips', function (Blueprint $table) {
-            $table->string('date');
+            $table->string('date')->nullable();
         });
     }
 
